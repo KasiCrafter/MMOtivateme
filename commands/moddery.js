bebaba33e1d3f1352d9e830cb98f9c6f3b40f3a4
Glitch (mmotivate) <none>
2018-07-15 04:13:08 +0000

    Test SQLite3 message works.

--- commands/moddery.js
+++ commands/moddery.js
@@ -1,8 +1,10 @@
 exports.run = (client, message, [mention, ...reason]) => {
   const modRole = message.guild.roles.find("name", "Mods") || "NERDS";
   
-  message.channel.send(modRole);
-  if (modRole) {
-    return console.log("The MODS role does not exist.");
+
+  if (!modRole) {
+   return console.log("The MODS role does not exist.");
   }
+  
+  message.channel.send(modRole);
 }
---
+++ heartbeat.js
@@ -0,0 +1,17 @@
+exports.run = () => {
+  const http = require('http');
+  const express = require('express');
+  const app = express();
+
+
+  app.get("/", (request, response) => {
+    console.log(Date.now() + " Ping Received");
+    response.sendStatus(200);
+  });
+  app.listen(process.env.PORT);
+  setInterval(() => {
+    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+  }, 240000);
+
+  console.log("Server Pinging Started using express...");
+}
